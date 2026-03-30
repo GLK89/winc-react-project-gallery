dd798af6ae0c0fdf04a1c7ae3762de79a0ae024b
Ginger Korporaal <gingerkorporaal@hotmail.com>
2026-03-30 22:02:46 +0200

    update header for dark mode

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -7,16 +7,21 @@ export const Header = () => {
       position="sticky"
       top={0}
       zIndex="sticky"
-      bg="white"
+      bg={{ _light: "gray.600", _dark: "gray.800" }}
       boxShadow="sm"
       px={6}
       py={4}
     >
       <Flex justify="space-between" align="center">
-        <Heading size="md">My Portfolio</Heading>
+        <Heading size="md" color="white">
+          {" "}
+          My Portfolio
+        </Heading>
 
         <Flex gap={3}>
-          <Button variant="ghost">Home</Button>
+          <Button variant="ghost" color="white">
+            Home
+          </Button>
           <Button variant="ghost">Projects</Button>
           <Button variant="ghost">About</Button>
           <Button variant="ghost">Contact</Button>
