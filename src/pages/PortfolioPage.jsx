7bc76192e91bbd3e983188c9dd15f108f98aa081
Ginger Korporaal <gingerkorporaal@hotmail.com>
2026-03-30 22:09:53 +0200

    improve page background and spacing for better dark mode layout

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -3,8 +3,15 @@ import { PortfolioItemCard } from "../components/ui/PortfolioItemCard";
 
 export const PortfolioPage = ({ portfolioItems, setSelectedItem }) => {
   return (
-    <Stack p={6} spacing={6}>
-      <Heading size="lg">Portfolio Page</Heading>
+    <Stack
+      p={6}
+      spacing={6}
+      bg={{ _light: "gray.50", _dark: "gray.900" }}
+      minH="100vh"
+    >
+      <Heading size="lg" color={{ _light: "black", _dark: "white" }}>
+        Portfolio Page
+      </Heading>
 
       <SimpleGrid columns={{ base: 1, md: 2, xl: 3 }} gap={6}>
         {portfolioItems.map((item) => (
